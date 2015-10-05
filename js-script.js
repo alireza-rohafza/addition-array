41482c8fae1f9c13c4f23ce08750dcd6d2922164
Rohafza <alirezarohafza@yahoo.com>
2015-10-05 22:30:30 +0330

    Update js-script.js

--- js-script.js
+++ js-script.js
@@ -1,5 +1,14 @@
-var number1 = new Array(m);
-var number2 = new Array(n);
+function func1() {
+var numb1 = document.getElementById("num1").value;
+var numb2 = document.getElementById("num2").value;
+var number1=new Array(1);
+var number2=new Array(1);
+/*for(var a=0)
+hanooz kamel nist
+
+*/
+m=number1.length;
+n=number2.length;
 while (n > m) {
         number1.unshift(0);
 }
@@ -23,4 +32,5 @@ if (sum[0] == 0) {
 }
 for (var j = 0; j < sum.length; j++) {
     document.write(sum[j])
+}
 }
